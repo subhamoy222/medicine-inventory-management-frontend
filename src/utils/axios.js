8928b5d91389c82a5df9b232b585aec7c53c6813
Subhamoy Sasmal <2023aspire117@gmail.com>
2025-05-07 22:55:53 +0530

    frontend link updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -15,6 +15,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
+    console.log('Token in localStorage:', token); // Add this line for debugging
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
